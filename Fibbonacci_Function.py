1ac702aceba45528c5d74efe755f3a271700699f
Chris Sanders <christopher.sanders78@outlook.com>
2021-05-20 10:47:35 -0400

    Add files via upload

---
+++ Fibbonacci_Function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def fib(var=1000000):    
+    num=[0,1]
+    sum = 0
+    while num[-1] <var:
+        sum = num[-1]+num[-2]
+        num.append(sum)
+    else:
+        return(num)
+
